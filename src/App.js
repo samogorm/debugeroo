fe4b38a34932a0c5442333760e4b7f6361da5aad
sam.ogorman <sam.ogorman@onthebeach.co.uk>
2020-02-22 11:44:27 +0000

    Add food images/assets

--- src/App.js
+++ src/App.js
@@ -3,20 +3,9 @@ import React from 'react';
 import Button from './components/button/button';
 
 const App = () => (
-  <div className="App">
+  <div className="app">
     <Button
       label="Test Button"
-      action={ () => console.log('Clicked')}
-    />
-    <Button
-      label="Test Button"
-      colour="light"
-      action={ () => console.log('Clicked')}
-    />
-     <Button
-      label="Test Button"
-      colour="danger"
-      type="block"
       action={ () => console.log('Clicked')}
     />
   </div>
