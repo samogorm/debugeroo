531d609f7c259ded278133679ac06fdab522b5c3
sam.ogorman <sam.ogorman@onthebeach.co.uk>
2020-02-22 11:05:03 +0000

    React app setup

---
+++ src/components/button/button.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './button.scss';
+
+const Button = ({ label, type, icon, action }) => (
+  <button onClick={ action } className={`button button--${type}`}>
+    <span className="button__label">
+      { icon && (
+        <span>
+          { icon }
+        </span>
+      )}
+      { label }
+    </span>
+  </button>
+);
+
+Button.defaultProps = {
+  type: 'primary',
+};
+
+Button.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  icon: PropTypes.string,
+  action: PropTypes.func,
+};
+
+export default Button;
